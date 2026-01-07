dab338f25ebda5e436e00260eb8f74df88f02939
MartiValverde <u1994928@campus.udg.edu>
2026-01-07 19:47:31 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+function Add_FIPORC() {
+    document.getElementById("fiporc_booked").style.display = "list-item";
+    alert("FIPORC booked successfully!");
+}
+
+function Add_Via() {
+    document.getElementById("via_booked").style.display = "list-item";
+    alert("Via Verda booked successfully!");
+}
+
+function Add_Mona() {
+    document.getElementById("mona_booked").style.display = "list-item";
+    alert("Fundació Mona booked successfully!");
+}
+
+function Remove_FIPORC() {
+    document.getElementById("fiporc_booked").style.display = "none";
+    alert("FIPORC removed successfully!");
+}
+
+function Remove_Via() {
+    document.getElementById("via_booked").style.display = "none";
+    alert("Via Verda removed successfully!");
+}
+
+function Remove_Mona() {
+    document.getElementById("mona_booked").style.display = "none";
+    alert("Fundació Mona removed successfully!");
+}
